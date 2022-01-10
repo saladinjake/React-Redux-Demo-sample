24a5c0b74c4e855b31c59062da5470fb4786d6be
saladinjake <juwavictor@gmail.com>
2022-01-10 19:07:17 +0100

    fix bugs on component props

--- src/components/SampleProductContainer.js
+++ src/components/SampleProductContainer.js
@@ -2,10 +2,10 @@ import React from "react";
 import { connect } from "react-redux";
 import { cart } from "../redux";
 
-const SampleProductContainer = ({ numberOfItems, addToCart }) => {
+const SampleProductContainer = ({ cart_item, addToCart }) => {
   return (
     <div>
-      <h2>Total Items - {numberOfItems}</h2>
+      <h2>Total Items - { cart_item}</h2>
       <button onClick={addToCart}>Add To Cart</button>
     </div>
   );
