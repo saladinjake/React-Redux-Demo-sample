a64f0974e0da88d57b5a8df2da49c1c0772abfa3
saladinjake <juwavictor@gmail.com>
2022-01-10 17:57:22 +0100

    bootstrap simple react application

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Provider } from "react-redux";
+import "./App.css";
+
+function App() {
+  return (
+    <Provider>
+      <div className="App">
+        
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
